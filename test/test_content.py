e1c61a12d0550e64b148381b8950e402e55f1ab5
HU-Lee <haulrest@gmail.com>
2024-03-07 11:25:34 +0900

    Delete code

--- test/test_content.py
+++ test/test_content.py
@@ -1,49 +1,10 @@
-import glob
 import os
-import shutil
 import json
-from hub.process.markdown import md_to_text, codecov_preprocess
-import hub.checking.docs.en as en_docs
-import hub.checking.docs.ko as ko_docs
+from hub.process.markdown import codecov_preprocess
 from hub.making.readme import make_readme
 from hub.util.files import get_text_from_file, get_text_from_url
 
 CONTENT_COMMENT = "<!-- Content -->"
-
-def test_linkedin_profile():
-    for file in glob.glob('content-personal/linkedin/*.md'):
-        text = get_text_from_file(file)
-        en_docs.check_capitalization_on_content(text)
-
-def test_disquiet_profile():
-    shutil.rmtree('text/disquiet', ignore_errors=True)
-    os.makedirs('text/disquiet', exist_ok=True)
-    for file in glob.glob('content-personal/disquiet/*.md'):
-        file_name = file.split(os.sep)[-1].replace('.md', '')
-        text = get_text_from_file(file)
-        ko_docs.check_english_words(raw_content=text, name=file_name, folder='disquiet')
-
-def test_peerlist_profile():
-    for file in glob.glob('content-personal/peerlist/*.md'):
-        readme_sentences = get_text_from_file(file).split('\n')
-        for sentence in readme_sentences:
-            en_docs.check_capitalization(sentence)
-
-def test_blog_posts():
-    shutil.rmtree('text/blog', ignore_errors=True)
-    os.makedirs('text/blog', exist_ok=True)
-    for file in glob.glob('content-personal/blog/*.md'):
-        file_name = file.split(os.sep)[-1].replace('.md', '')
-        text = get_text_from_file(file)
-        ko_docs.check_english_words(raw_content=text, name=file_name, folder='blog')
-
-def test_wiki_posts():
-    shutil.rmtree('text/wiki', ignore_errors=True)
-    os.makedirs('text/wiki', exist_ok=True)
-    for file in glob.glob('content-personal/wiki/*.md'):
-        file_name = file.split(os.sep)[-1].replace('.md', '')
-        text = get_text_from_file(file)
-        ko_docs.check_english_words(raw_content=text, name=file_name, folder='wiki')
 
 def test_repo_readme():
     os.makedirs('result/readme', exist_ok=True)
@@ -66,10 +27,3 @@ def test_repo_readme():
             online_content = codecov_preprocess(online_content)
         assert online_content.startswith(content_prefix) 
         assert online_content.endswith(content_suffix)
-
-        # Check content
-        readme_sentences = get_text_from_file(f'content-personal/repo-readme/{name}.md').split('\n')
-        online_text = md_to_text(online_content)
-        for sentence in readme_sentences:
-            assert sentence in online_text
-            en_docs.check_capitalization(sentence)
